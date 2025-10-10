ce4a25ed2423d94f2f574e63b8941900dd457b00
Frizlab <fload@me.com>
2025-10-10 10:00:53 +0200

    Add more tests

--- Tests/ConfVariablesTests.swift
+++ Tests/ConfVariablesTests.swift
@@ -18,6 +18,10 @@ struct ConfVariablesTests {
 		#expect(try ConfVariables.resolveVariables(in: #"yolo${yolo}yolo"#, localVars: ["yolo": Data("hey!".utf8)]) == Data("yolohey!yolo".utf8))
 		#expect(try ConfVariables.resolveVariables(in: #"yolo${env:YOLO}\$"#, localVars: ["YOLO": Data("hey!".utf8)]) == Data("yoloyolenv$".utf8))
 		#expect(try ConfVariables.resolveVariables(in: #"yolo${env:YOLO}\$${YOLO}"#, localVars: ["YOLO": Data("hey!".utf8)]) == Data("yoloyolenv$hey!".utf8))
+		#expect(try ConfVariables.resolveVariables(in: #"${local:${env:YOLO}}"#, localVars: ["yolenv": Data("hey!".utf8)]) == Data("hey!".utf8))
+		#expect(try ConfVariables.resolveVariables(in: #"${${YOLO}}"#, localVars: ["YOLO": Data("subyolo".utf8), "subyolo": Data("hey!".utf8)]) == Data("hey!".utf8))
+		#expect(try ConfVariables.resolveVariables(in: #"${${YOLO}}"#, localVars: ["YOLO": Data("local:subyolo".utf8), "subyolo": Data("hey!".utf8)]) == Data("hey!".utf8))
+		#expect(try ConfVariables.resolveVariables(in: #"${local:sub${YOLO}}"#, localVars: ["YOLO": Data("yolo".utf8), "subyolo": Data("hey!".utf8)]) == Data("hey!".utf8))
 	}
 	
 	@Test
@@ -27,6 +31,8 @@ struct ConfVariablesTests {
 		#expect(throws: Err.self, performing: { try ConfVariables.resolveVariables(in: #"$\{abc}"#, localVars: ["abc": Data("yolo".utf8)]) as Data })
 		#expect(throws: Err.self, performing: { try ConfVariables.resolveVariables(in: #"${env:NOTEXISTING}"#, localVars: [:]) as Data })
 		#expect(throws: Err.self, performing: { try ConfVariables.resolveVariables(in: #"${env:YOLO}"#, localVars: [:], allowEnvVars: false) as Data })
+		#expect(throws: Err.self, performing: { try ConfVariables.resolveVariables(in: #"${invalid-domain:YOLO}"#, localVars: [:], allowEnvVars: false) as Data })
+		#expect(throws: Err.self, performing: { try ConfVariables.resolveVariables(in: #"${${YOLO}}"#, localVars: ["YOLO": Data("invalid-domain:subyolo".utf8), "subyolo": Data("hey!".utf8)]) as Data })
 	}
 	
 }
